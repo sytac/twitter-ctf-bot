a0ccf055fd47b9bb182e6bbfbf1c44963544539a
Tonino Catapano <info@toninocatapano.com>
2015-10-23 15:52:24 +0200

    progress
    Added DM handling (in/out), follows

--- src/main/java/com/sytac/twitter_ctf_bot/Bot.java
+++ src/main/java/com/sytac/twitter_ctf_bot/Bot.java
@@ -1,5 +1,6 @@
 package com.sytac.twitter_ctf_bot;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -10,6 +11,7 @@ import java.util.concurrent.LinkedBlockingQueue;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.mashape.unirest.http.Unirest;
 import com.twitter.hbc.ClientBuilder;
 import com.twitter.hbc.core.Client;
 import com.twitter.hbc.core.Constants;
@@ -19,6 +21,10 @@ import com.twitter.hbc.core.endpoint.UserstreamEndpoint;
 import com.twitter.hbc.core.processor.StringDelimitedProcessor;
 import com.twitter.hbc.httpclient.auth.Authentication;
 import com.twitter.hbc.httpclient.auth.OAuth1;
+
+import twitter4j.Twitter;
+import twitter4j.TwitterFactory;
+import twitter4j.conf.ConfigurationBuilder;
 
 
 
@@ -30,22 +36,103 @@ public class Bot {
 	private static final Properties CONF_FILE = new Properties();
 	
 	private static Client hosebirdClient;
-	private static Integer participantNumber;
+	private static Integer participantNumber = 0;
+	private static Twitter twitter4jClient;
+	
 	
 	public Bot(){}
 	
 	private void run(String path){
+		//ShutDownHook to catch the SIGING when terminating the process.
 		Runtime.getRuntime().addShutdownHook(new Thread(){
             @Override
             public void run(){
                 LOGGER.info("Shutdown hook catched, closing the hosebirdClient");
-                LOGGER.info("TOTAL NUMBER OF PARTECIPANTS FOR THIS SESSIO: " + participantNumber);
+                LOGGER.info("Number of participants registered for this session: " + participantNumber);
                 hosebirdClient.stop();
-                
+                try {
+					Unirest.shutdown();
+				} catch (IOException e) {
+					 LOGGER.error("error while shutting down Unirest Client: ", e);
+				}
             }
-        });
+        });	
 		
+		loadPropFile(path);	//load the properties file
 		try{
+			//get the keys and tokens from the CONF_FILE
+			String consumerKey = CONF_FILE.getProperty("consumerKey");
+			String consumerSecret = CONF_FILE.getProperty("consumerSecret");
+			String token = CONF_FILE.getProperty("token");
+			String secret = CONF_FILE.getProperty("secret");
+			
+			/** Set up the blocking queue for hbc: size based on expected TPS of your stream */
+			BlockingQueue<String> msgQueue = new LinkedBlockingQueue<String>(1000);
+			/** Declare the host you want to connect to, the endpoint, and authentication (basic auth or oauth) */
+			Hosts hosebirdHosts = new HttpHosts(Constants.USERSTREAM_HOST);
+			
+			initializeHBC(consumerKey, consumerSecret, token, secret, msgQueue, hosebirdHosts);
+			initializeTwit4j(consumerKey, consumerSecret, token, secret);
+			
+			hosebirdClient.connect();// Attempts to establish a connection to the Sytac's user stream.
+			new ReadingThread(hosebirdHosts, msgQueue, hosebirdClient, twitter4jClient, participantNumber).start(); //Run the Thread that will consume the User-stream
+		}catch(Exception e){
+			LOGGER.error(e.getMessage(),e);
+			LOGGER.info("Unexpected error encoutered, closing the connection...");
+			hosebirdClient.stop();
+		}
+	}
+	
+	/**
+	 * Initialize the HoseBird Client (STREAMING-API part)
+	 * @param consumerKey
+	 * @param consumerSecret
+	 * @param token
+	 * @param secret
+	 * @param msgQueue
+	 * @param hosebirdHosts
+	 */
+	private void initializeHBC(String consumerKey,String consumerSecret,String token,String secret,BlockingQueue<String> msgQueue, Hosts hosebirdHosts){		
+		UserstreamEndpoint userEndpoint = new UserstreamEndpoint();
+		userEndpoint.withUser(true); //fetch only the user-related messages	
+		
+		Authentication hosebirdAuth = new OAuth1(consumerKey, consumerSecret, token, secret);
+		ClientBuilder builder = new ClientBuilder()
+				  .name("Hosebird-Client-01")                              // optional: mainly for the logs
+				  .hosts(hosebirdHosts)
+				  .authentication(hosebirdAuth)
+				  .endpoint(userEndpoint)
+				  .processor(new StringDelimitedProcessor(msgQueue));
+				 // .eventMessageQueue(eventQueue);                          // optional: use this if you want to process client events
+
+		hosebirdClient = builder.build();
+	}
+	
+	/**
+	 * Initialize the Twitter4j Client instance (REST-API part)
+	 * @param consumerKey
+	 * @param consumerSecret
+	 * @param token
+	 * @param secret
+	 */
+	private void initializeTwit4j(String consumerKey, String consumerSecret,String token, String secret){
+		ConfigurationBuilder cb = new ConfigurationBuilder();
+		cb.setDebugEnabled(true)
+		  .setOAuthConsumerKey(consumerKey)
+		  .setOAuthConsumerSecret(consumerSecret)
+		  .setOAuthAccessToken(token)
+		  .setOAuthAccessTokenSecret(secret);
+		TwitterFactory tf = new TwitterFactory(cb.build());		
+		twitter4jClient = tf.getInstance();
+	}
+	
+	
+	/**
+	 * Load the properties file
+	 * @param path
+	 */
+	private void loadPropFile(String path){
+		try {
 			InputStream in = Files.newInputStream(Paths.get(path));
 			if (path == null || path.isEmpty() || in == null){
 				LOGGER.error("Please specificate a valid path for the properties file in the first argument");
@@ -53,42 +140,14 @@ public class Bot {
 			}
 			CONF_FILE.load(in);
 			in.close();
-			String consumerKey = CONF_FILE.getProperty("consumerKey");
-			String consumerSecret = CONF_FILE.getProperty("consumerSecret");
-			String token = CONF_FILE.getProperty("token");
-			String secret = CONF_FILE.getProperty("secret");
-			
-			
-			/** Set up your blocking queues: Be sure to size these properly based on expected TPS of your stream */
-			BlockingQueue<String> msgQueue = new LinkedBlockingQueue<String>(100000);
-			/** Declare the host you want to connect to, the endpoint, and authentication (basic auth or oauth) */
-			Hosts hosebirdHosts = new HttpHosts(Constants.USERSTREAM_HOST);
-			UserstreamEndpoint userEndpoint = new UserstreamEndpoint();
-			userEndpoint.withUser(true); //fetch only the user-related messages
-			
-			// These secrets should be read from a config file
-			Authentication hosebirdAuth = new OAuth1(consumerKey, consumerSecret, token, secret);
-			ClientBuilder builder = new ClientBuilder()
-					  .name("Hosebird-Client-01")                              // optional: mainly for the logs
-					  .hosts(hosebirdHosts)
-					  .authentication(hosebirdAuth)
-					  .endpoint(userEndpoint)
-					  .processor(new StringDelimitedProcessor(msgQueue));
-					 // .eventMessageQueue(eventQueue);                          // optional: use this if you want to process client events
-	
-			hosebirdClient = builder.build();
-			// Attempts to establish a connection.
-			hosebirdClient.connect();
-			//Run the Thread that will consume the User-stream
-			new ReadingThread(hosebirdHosts, msgQueue, hosebirdClient, participantNumber).start();
-		}catch(Exception e){
-			LOGGER.error(e.getMessage(),e);
-			LOGGER.info("Closing the connection...");
-			hosebirdClient.stop();
+		} catch(IOException e) {
+			LOGGER.error("Error while reading the properties file: "+ path, e);
+			return;
 		}
 	}
 	
+	
 	public static void main(String[] args) {
 		new Bot().run(args[0]);
-	}		
+	}	
 }
--- src/main/java/com/sytac/twitter_ctf_bot/ReadingThread.java
+++ src/main/java/com/sytac/twitter_ctf_bot/ReadingThread.java
@@ -11,27 +11,47 @@ import org.slf4j.LoggerFactory;
 import com.twitter.hbc.core.Client;
 import com.twitter.hbc.core.Hosts;
 
+import twitter4j.DirectMessage;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+
 public class ReadingThread extends Thread{
 
 	final static Logger LOGGER = LoggerFactory.getLogger(Bot.class);
 	final static String WELCOME_MESSAGE_PARTICIPANT = "Welcome to the Capture the flag competition from the Sytac team, have fun!";
+	final static String COULDNOT_FOLLOW_MESSAGE = "Seems you were already signed to the Capture the flag competition";
 	final static String RIGHT_ANSWER_MESSAGE = "Great!";
 	final static String WRONG_ANSWER_MESSAGE = "Too Bad :/";
+	final static String WINNER_MESSAGE = "Congratulations, you are the winner of an amazing Parrot AR Drone 2.0!";
 	
+	final static String TWITTER_DM_ENDPOINT = "https://api.twitter.com/1.1/direct_messages/new.json";
+	final static String SYTAC_REST_ENDPOINT = "http://sytac.io/ctf.do";
+	final static String FLAG_KEY = "foundFlag";
+	final static String PARTIC_ID_KEY = "partecipantId";
+	final static String PARTIC_NAME_KEY = "partecipantName";
+	
+	final static long SYTAC_USER_ID = 194054816;
 	
 	Hosts _hosebirdHosts;
 	BlockingQueue<String> _msgQueue;
 	Client _hosebirdClient;
+	Twitter _twitter4jClient;
 	Integer _partecipantsCount;
 	
-	public ReadingThread(Hosts hosebirdHosts, BlockingQueue<String> msgQueue, Client hosebirdClient, Integer partecipantsNumber){
+	public ReadingThread(Hosts hosebirdHosts, BlockingQueue<String> msgQueue, Client hosebirdClient, Twitter twitter4jClient, Integer partecipantsNumber){
 		_hosebirdClient = hosebirdClient;
 		_msgQueue = msgQueue;
 		_hosebirdClient = hosebirdClient;
 		_partecipantsCount = partecipantsNumber;
+		_twitter4jClient = twitter4jClient;
 	}
 	
-	 public void run(){
+	
+	
+	/**
+	 * The loop that will intercept and handle the incoming messages for Sytac
+	 */
+	public void run(){
 		try {
 			while (!_hosebirdClient.isDone()) {
 				  String msg = _msgQueue.take();
@@ -43,52 +63,112 @@ public class ReadingThread extends Thread{
 		}
 	 }
 	 
-	 
+	
+	
+	
+	 /**The processing routine: handle mentions and DM receiving and answers
+	  * 
+	  * @param json
+	  */
 	 private void processMessage(String json){
-		//print the raw json
-		LOGGER.info(json); //SET TO DEBUG!!!!
-		//System.out.println(msg);
+		LOGGER.debug(json); //SET TO DEBUG!!!!
         ObjectMapper mapper = new ObjectMapper();
-        // use the ObjectMapper to read the json string and create a tree
-        JsonNode node;
 		try {
-			//Marshalling of the JSON
-			node = mapper.readTree(json);
+			//Unmarshalling of the JSON
+			JsonNode node = mapper.readTree(json);
 			JsonNode mention = node.path("entities").path("user_mentions");
 			JsonNode mention_text = node.path("text");
-			JsonNode direct_message = node.path("direct_message").path("text");
-			if(mention.isValueNode() && mention_text.isValueNode()){ //case of mention
-				_partecipantsCount++;
-				LOGGER.info("Received message: "+ mention_text.getTextValue());
-				JsonNode participant = node.path("user");
-				followParticipant(participant);
-				DM(participant.path("id").getTextValue(), WELCOME_MESSAGE_PARTICIPANT);
-			}else if(direct_message.isValueNode()){
-				//HANDLE DM
-			}else{
-				LOGGER.warn("The JSON received isn't a well formed DM");
+			JsonNode direct_msg = node.path("direct_message").path("text");
+			String direct_msgStr = direct_msg.getTextValue();
+			JsonNode direct_msg_senderId = node.path("direct_message").path("sender").path("id");
+			JsonNode name = node.path("direct_message").path("sender").path("screen_name");
+			JsonNode event_node = node.path("event");
+			JsonNode participant = node.path("user");
+			
+			if(event_node.isMissingNode() && //if the event node is not present in the message
+				!mention.isMissingNode() && //if mention node is present in the message
+				mention_text.isValueNode() && //if mention text is present in the message
+				mention_text.getTextValue().toLowerCase().contains("#ctf")) //if mention text contains the CTF flag
+			{ //CASE OF A MENTION: try to follow the user and add him to the participants	
+				LOGGER.info("Received mention: " + mention_text.getTextValue());		
+				//follow the participant
+				boolean followSuccess = followParticipant(participant.path("id").getLongValue());
+				// send him/her a welcome DM or a DM informing he's/she's already in the competition
+				dm(participant.path("id").getLongValue(), followSuccess ? WELCOME_MESSAGE_PARTICIPANT : COULDNOT_FOLLOW_MESSAGE); 
+				if(followSuccess) _partecipantsCount++;
+			}else if(direct_msg.isValueNode() && //if the direct_msg node is present in the message
+					direct_msgStr.toLowerCase().startsWith("#ctf") && //if direct_msg contains the CTF flag
+					!direct_msg_senderId.isMissingNode() && //if direct_msg_senderId is present in the message
+					direct_msg_senderId.getLongValue() != SYTAC_USER_ID) //if the received message is not an echo message (message from Sytac itself) 
+			{ //CASE of a DM for the competition
+				boolean ok = processAnswerToRemote((direct_msgStr.toLowerCase().split("#ctf")[1]).trim(), name.getTextValue(), direct_msg_senderId.getLongValue());
+				dm(participant.path("id").getLongValue(), ok ? RIGHT_ANSWER_MESSAGE : WRONG_ANSWER_MESSAGE); 
+			}else{ //OTHER MESSAGES RECEIVED: skip them
+				LOGGER.warn("The JSON received isn't a ctf-related message: " + node.toString());
 				return;
 			}
 
-			//DO SOMETHING WITH THE RECEIVED MESSAGE
-	        System.out.println(direct_message.getTextValue());
 		} catch (IOException e) {
 			LOGGER.error(e.getMessage(), e);
 		}    
 	 }
 	 
 	 
-	 private void followParticipant(JsonNode participant){
-		String idParticipant = participant.path("id").getTextValue();
-		
+	 private boolean followParticipant(long idParticipant){
+		try {
+			_twitter4jClient.createFriendship(idParticipant);
+		} catch (TwitterException e) {
+			LOGGER.error(e.getMessage(), e);
+			return false;
+		}
+		return true;
 	 }
 	 
-	 private boolean DM(String idUser, String message){
-		 return false;
+	 /**
+	  * Send a private message to the participant 
+	  * @param idUser
+	  * @param message
+	  * @return
+	  */
+	 private void dm(long idUser, String message){
+		 try {
+			 DirectMessage msg = _twitter4jClient.sendDirectMessage(idUser, message);
+			 LOGGER.info("Sent: " + msg.getText() + " to @" + msg.getRecipientScreenName());
+		 } catch (TwitterException e) {
+			LOGGER.error("Error during the DM to the partecipant " + idUser + ": TWITTER4J exception ", e);
+		 }	 
 	 }
 	 
-	 private boolean processAnswerToQuiz(String quizNr, String answer){	 
-		 //HTTP CALL TO A REMOTE REST SERVICE
+
+	 
+	 
+	 /**
+	  * Send the received answer for the quiz {@value quizNr} to the Sytac webapp for checking
+	  * @param flag
+	  * @param partecipantName
+	  * @param partecipantId
+	  * @return
+	  */
+	 private boolean processAnswerToRemote(String flag, String partecipantName, long partecipantId ){	 
+		 /*
+		  try {
+			 
+			HttpResponse<String> a = Unirest.post(SYTAC_REST_ENDPOINT)
+			  //.queryString("quizNr", quizNr)
+			  .field(FLAG_KEY, flag)
+			  .field(PARTIC_ID_KEY, partecipantId)
+			  .field(PARTIC_NAME_KEY, partecipantName)
+			  .asString();
+			  //.asJson();
+			if(a.getStatus() == 200) 
+				return Boolean.valueOf(a.getBody());
+			return false;		
+		} catch (UnirestException e) {
+			LOGGER.error("Error during the HTTP-POST to the Sytac-webapp", e);
+			
+			return false;
+			
+		}*/
 		 return false;
 	 }
 	 
