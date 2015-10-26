61a053a8bfc6729cf27f437e3bfce5fd3cf644a1
Carlo Sciolla <skuro@skuro.tk>
2015-10-26 18:25:28 +0100

    Moved initialization to the App level

--- src/main/java/com/sytac/twitter_ctf_bot/Bot.java
+++ src/main/java/com/sytac/twitter_ctf_bot/Bot.java
@@ -1,24 +1,13 @@
 package com.sytac.twitter_ctf_bot;
 
 import com.mashape.unirest.http.Unirest;
-import com.twitter.hbc.ClientBuilder;
 import com.twitter.hbc.core.Client;
-import com.twitter.hbc.core.Constants;
-import com.twitter.hbc.core.Hosts;
-import com.twitter.hbc.core.HttpHosts;
-import com.twitter.hbc.core.endpoint.UserstreamEndpoint;
-import com.twitter.hbc.core.processor.StringDelimitedProcessor;
-import com.twitter.hbc.httpclient.auth.Authentication;
-import com.twitter.hbc.httpclient.auth.OAuth1;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import twitter4j.Twitter;
-import twitter4j.TwitterFactory;
-import twitter4j.conf.ConfigurationBuilder;
 
 import java.io.IOException;
 import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.LinkedBlockingQueue;
 
 /**
  * A twitter Bot which manages the communication with the participants of the Sytac Capture The Flag competition
@@ -32,31 +21,27 @@ public class Bot {
     private final static Logger LOGGER = LoggerFactory.getLogger(Bot.class);
 
     private final Configuration config;
+    private final BlockingQueue<String> queue;
 
-    private Client hosebirdClient;
+    private final Client hosebirdClient;
     private Integer participantNumber = 0;
-    private Twitter twitter;
+    private final Twitter twitter;
     private ReadingThread reader;
 
-    public Bot(Configuration configuration) {
+    public Bot(Configuration configuration, Twitter twitterClient, Client streamClient, BlockingQueue<String> queue) {
         this.config = configuration;
+        this.hosebirdClient = streamClient;
+        this.twitter = twitterClient;
+        this.queue = queue;
     }
 
     public void run() {
-        /** Set up the blocking queue for hbc: size based on expected TPS of your stream */
-        BlockingQueue<String> msgQueue = new LinkedBlockingQueue<>(1000);
-
-        /** Declare the host you want to connect to, the endpoint, and authentication (basic auth or oauth) */
-        Hosts hosebirdHosts = new HttpHosts(Constants.USERSTREAM_HOST);
 
         try {
-            hosebirdClient = initializeHBC(msgQueue, hosebirdHosts);
-            twitter = initializeTwit4j();
-
             gracefulShutdown();
 
             hosebirdClient.connect(); // Attempts to establish a connection to the Sytac's user stream.
-            reader = new ReadingThread(msgQueue, hosebirdClient, twitter, participantNumber, config);
+            reader = new ReadingThread(queue, hosebirdClient, twitter, participantNumber, config);
             new Thread(reader).start(); //Run the Thread that will consume the User-stream
 
             // TODO: process messages in an infinite loop here..
@@ -90,43 +75,5 @@ public class Bot {
         });
     }
 
-    /**
-     * Initialize the HoseBird Client (STREAMING-API part)
-     *
-     * @param msgQueue
-     * @param hosebirdHosts
-     */
-    private Client initializeHBC(BlockingQueue<String> msgQueue, Hosts hosebirdHosts) {
-        UserstreamEndpoint userEndpoint = new UserstreamEndpoint();
-        userEndpoint.withUser(true); //fetch only the user-related messages
 
-        Authentication hosebirdAuth = new OAuth1(config.getConsumerKey(),
-                                                 config.getConsumerSecret(),
-                                                 config.getToken(),
-                                                 config.getSecret());
-
-        ClientBuilder builder = new ClientBuilder()
-                .name(config.getBotName())
-                .hosts(hosebirdHosts)
-                .authentication(hosebirdAuth)
-                .endpoint(userEndpoint)
-                .processor(new StringDelimitedProcessor(msgQueue));
-
-        return builder.build();
-    }
-
-    /**
-     * Initialize the Twitter4j Client instance (REST-API part)
-     */
-    private Twitter initializeTwit4j() {
-        ConfigurationBuilder cb = new ConfigurationBuilder();
-        cb.setDebugEnabled(true)
-                .setOAuthConsumerKey(config.getConsumerKey())
-                .setOAuthConsumerSecret(config.getConsumerSecret())
-                .setOAuthAccessToken(config.getToken())
-                .setOAuthAccessTokenSecret(config.getSecret());
-        TwitterFactory tf = new TwitterFactory(cb.build());
-
-        return tf.getInstance();
-    }
 }
